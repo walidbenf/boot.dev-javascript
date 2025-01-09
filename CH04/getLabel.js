e2b34be92b9b2e49bc7ea9e85fa5ea5884b3b935
walidbenf <45196746+walidbenf@users.noreply.github.com>
2025-01-09 22:05:38 +0100

    Create getLabel.js

---
+++ CH04/getLabel.js
@@ -0,0 +1,26 @@
+function getLabel(numStars) {
+  if (numStars >= 8 && numStars <= 10)
+    return "great"
+  else if (numStars >= 4 && numStars <= 7)
+    return "okay"
+  else if (numStars <= 3)
+    return "awful"
+}
+
+// don't touch below this line
+
+function test(numStars) {
+  console.log(`Stars=${numStars}, The movie was ${getLabel(numStars)}`)
+}
+
+test(10)
+test(9)
+test(8)
+test(7)
+test(6)
+test(5)
+test(4)
+test(3)
+test(2)
+test(1)
+test(0)
