f80c7e1e0894f2c88caf43e4b1a7fa9bb874cd58
walidbenf <45196746+walidbenf@users.noreply.github.com>
2025-01-10 15:15:16 +0100

    Create printClean.js

---
+++ CH06/printClean.js
@@ -0,0 +1,13 @@
+const printCleanReviews = (reviews, badWord) => {
+  for(let i = 0; i < reviews.length; i++){
+    if(reviews[i].includes(badWord) != true)
+      console.log(`Clean review: ${reviews[i]}`);
+  }
+}
+
+// don't touch below this line
+
+printCleanReviews([ 'The movie sucks', 'I love it', 'What garbage', 'so sucky' ], 'suck')
+console.log('---')
+printCleanReviews([ 'The movie sucks', 'I love it', 'What darn crap', 'darn, so sucky' ], 'darn')
+console.log('---')
